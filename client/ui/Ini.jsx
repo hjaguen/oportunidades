d3791a9da5e7621fbca3b455955ada3b3940a10f
SyBr Len <sybr@openmailbox.org>
2017-10-18 00:55:38 +0200

    Consulta acceptada. Usant Apollo-Client.

--- client/ui/Ini.jsx
+++ client/ui/Ini.jsx
@@ -5,36 +5,126 @@ import {
   Link
 } from 'react-router-dom';
 
-const 
-    url = `http://localhost:4000/graphql`,
-    query = `query {
-        allSubcategories(
-            apiUrl: "http://api.colombiaespassion.net", 
-            pageId: "1", 
-            categoryId: "2"
-        ) {
-            categoriaId
-            nom_categoria
-        }
-    }`
-;
+// import ApolloClient, { HttpLink } from 'apollo-client-preset';
+import gql from 'graphql-tag';
+//
+// const client = new ApolloClient({
+//     link: new HttpLink({
+//         uri: 'https://localhost:4000/graphql'
+//     })
+// });
 
-fetch(url, { 
-    method: 'POST',
-    Accept: 'api_version=2',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ query })
-})
-    .then(response => response.json())
-    .then(data => {
-        console.dir('Here is the data: ', data);
-        //resultats = data;
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({
+    networkInterface: createNetworkInterface({
+        uri: `http://localhost:4000/graphql`
     })
-;
+})
+
+
+
+// const
+//     url = `http://localhost:4000/graphql`,
+//     query = `query {
+//         allSubcategories(
+//             apiUrl: "http://api.colombiaespassion.net",
+//             pageId: "1",
+//             categoryId: "2"
+//         ) {
+//             categoriaId
+//             nom_categoria
+//         }
+//     }`
+// ;
+
+// fetch(url, {
+//     method: 'POST',
+//     Accept: 'api_version=2',
+//     headers: {
+//         'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify({ query })
+// })
+//     .then(response => response.json())
+//     .then(data => {
+//         console.dir('Here is the data: ', data);
+//         //resultats = data;
+//     })
+// ;
 
 //console.dir('Resultats: ', resultats);
+
+function queryAmbVariables( varsObj ) {
+    return client.query({
+        query: gql`
+            query Subcategories(
+                $apiUrl: String,
+                $pageId: String,
+                $categoryId: String,
+                $subcategoryId: String,
+                $sizeId: String,
+                $brandId: String,
+                $colorId: String
+            ) {
+                tallaSubcategoriaMARQUES(
+                    apiUrl: $apiUrl,
+                    pageId: $pageId,
+                    categoryId: $categoryId,
+                    subcategoryId: $subcategoryId,
+                    sizeId: $sizeId
+                ) {
+                    marcaId
+                    nom_marca
+                }
+                marcaSubcategoriaCOLORS(
+                    apiUrl: $apiUrl,
+                    pageId: $pageId,
+                    categoryId: $categoryId,
+                    subcategoryId: $subcategoryId,
+                    brandId: $brandId
+                ) {
+                    colorId
+                    nom_color
+                    label_color
+                }
+                colorSUBCATEGORIES(
+                    apiUrl: $apiUrl,
+                    pageId: $pageId,
+                    categoryId: $categoryId,
+                    colorId: $colorId
+                ) {
+                    categoriaId
+                    nom_categoria
+                }
+                colorSubcategoriaMarcaTALLES(
+                    apiUrl: $apiUrl,
+                    pageId: $pageId,
+                    categoryId: $categoryId,
+                    subcategoryId: $subcategoryId,
+                    colorId: $colorId,
+                    brandId: $brandId
+                ) {
+                    tallaId
+                    nom_talla
+                    label_talla
+                    orden_talla
+                    publicar_talla
+                }
+        }
+     `,
+      variables: {
+          "apiUrl": varsObj.apiUrl,
+          "pageId": varsObj.pageId,
+          "categoryId": varsObj.categoryId,
+          "subcategoryId": varsObj.subcategoryId,
+          "sizeId": varsObj.sizeId,
+          "brandId": varsObj.brandId,
+          "colorId": varsObj.colorId
+      }
+    })
+}
 
 export default class IniComp extends Component {
     constructor(props) {
@@ -42,27 +132,42 @@ export default class IniComp extends Component {
     }
 
     render() {
+        queryAmbVariables({
+            apiUrl: "http://api.colombiaespassion.net",
+            pageId: "1",
+            categoryId: "2",
+            subcategoryId: "2",
+            sizeId: "21",
+            brandId: "4",
+            colorId: "17"
+        })
+          .then(data => console.log(data))
+          .catch(error => console.error(error));
+
         return (
-            <Router>
-                <div>
-                    <Route exact path="/" render={()=>(
-                        <div>
-                            <h1>Component INICIAL!</h1>
+            <ApolloProvider client={client}>
+                <Router>
+                    <div>
+                        <Route exact path="/" render={() => (
                             <div>
-                                <h2>Meeec</h2>
+                                <h1>Component INICIAL!</h1>
+                                <div>
+                                    <h2>Meeec</h2>
+                                </div>
                             </div>
-                        </div>
-                    )}/>
-                    <Route path="/subcategories" render={()=>(
-                        <ul>{
-                           xhr.response.map(
-                               (v,i,a) => <li key={i}>{v.data.allSubcategories.nom}</li>
-                           )
-                        }
-                        </ul>
-                    )}/>
-                </div>
-            </Router>
+                        )}/>
+                        <Route path="/query" render={() => (
+                            null
+                        )}/>
+                    </div>
+                </Router>
+            </ApolloProvider>
         );
     }
 };
+// <ul>{
+//    xhr.response.map(
+//        (v,i,a) => <li key={i}>{v.data.allSubcategories.nom}</li>
+//    )
+// }
+// </ul>
