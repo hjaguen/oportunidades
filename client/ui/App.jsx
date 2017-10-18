2d34e8a4a19354237aa7c30e3e4c703f258af39d
SyBr Len <sybr@openmailbox.org>
2017-10-18 16:05:19 +0200

    Mostrades imatges amb Masonry. Ara no fa scroll >.<'

--- client/ui/App.jsx
+++ client/ui/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import {
   BrowserRouter as Router,
   Route,
@@ -7,7 +8,6 @@ import {
 
 // import ApolloClient, { HttpLink } from 'apollo-client-preset';
 import gql from 'graphql-tag';
-import graphql from 'react-apollo';
 //
 // const client = new ApolloClient({
 //     link: new HttpLink({
@@ -16,7 +16,8 @@ import graphql from 'react-apollo';
 // });
 
 import ApolloClient, { createNetworkInterface } from 'apollo-client';
-import { ApolloProvider } from 'react-apollo';
+import { graphql, ApolloProvider } from 'react-apollo';
+import Masonry from 'react-masonry-component';
 
 const client = new ApolloClient({
     networkInterface: createNetworkInterface({
@@ -57,87 +58,81 @@ const client = new ApolloClient({
 
 //console.dir('Resultats: ', resultats);
 
-function queryAmbVariables( varsObj ) {
-    return client.query({
-        query: gql`
-            query Subcategories(
-                $apiUrl: String,
-                $pageId: String,
-                $categoryId: String,
-                $subcategoryId: String,
-                $sizeId: String,
-                $brandId: String,
-                $colorId: String
-            ) {
-                subcategoriaPRODUCTES(
-                    apiUrl: $apiUrl,
-                    pageId: $pageId,
-                    categoryId: $categoryId,
-                    subcategoryId: $subcategoryId,
-                    colorId: $colorId,
-                    brandId: $brandId,
-                    sizeId: $sizeId
-                ) {
-                    id
-		            referencia
-                    descripcion
-                    categoria
-                    marca
-                    precioBase
-                    precio2
-                    precio3
-                    precio4
-                    precioMiscelaneo
-                    proveedor
-                    descripcion_long_es
-                    nom_marca
-                    logo_marca
-                    nom_categoria
-                    imagen_principal
-                    gallery {
-                        id
-                        producto
-                        imagen
-                        imagen_min
-                        type
-                        ppal
-                    }
-                    galleryColors {
-                        id
-                        fotoId
-                        colorId
-                        num_color
-                        label_color
-                        imagen_min
-                    }
-              }
-        }
-     `,
-      variables: {
-          "apiUrl": varsObj.apiUrl,
-          "pageId": varsObj.pageId,
-          "categoryId": varsObj.categoryId,
-          "subcategoryId": varsObj.subcategoryId,
-          "sizeId": varsObj.sizeId,
-          "brandId": varsObj.brandId,
-          "colorId": varsObj.colorId
+const ProductesQuery = gql`
+    query Subcategories(
+        $apiUrl: String,
+        $pageId: String,
+        $categoryId: String,
+        $subcategoryId: String,
+        $sizeId: String,
+        $brandId: String,
+        $colorId: String
+    ) {
+        subcategoriaPRODUCTES(
+            apiUrl: $apiUrl,
+            pageId: $pageId,
+            categoryId: $categoryId,
+            subcategoryId: $subcategoryId,
+            colorId: $colorId,
+            brandId: $brandId,
+            sizeId: $sizeId
+        ) {
+            id
+            referencia
+            descripcion
+            categoria
+            marca
+            precioBase
+            precio2
+            precio3
+            precio4
+            precioMiscelaneo
+            proveedor
+            descripcion_long_es
+            nom_marca
+            logo_marca
+            nom_categoria
+            imagen_principal
+            gallery {
+                id
+                producto
+                imagen
+                imagen_min
+                type
+                ppal
+            }
+            galleryColors {
+                id
+                fotoId
+                colorId
+                num_color
+                label_color
+                imagen_min
+            }
       }
-    })
-}
+}`;
 
-const ProductesQuery = queryAmbVariables({
+let variables = {
     apiUrl: "http://api.colombiaespassion.net",
     pageId: "1",
     categoryId: "2",
-    subcategoryId: "2",
+    subcategoryId: "31",
     sizeId: "21",
     brandId: "4",
     colorId: "17"
-});
+};
 
 class Mostrari extends Component {
     constructor(props) {
         super(props);
+    }
+
+    static propTypes = {
+        data: PropTypes.shape({
+            loading: PropTypes.bool,
+            error: PropTypes.object,
+            subcategoriaPRODUCTES: PropTypes.array
+        }).isRequired
     }
 
     render() {
@@ -151,21 +146,53 @@ class Mostrari extends Component {
         }
 
         return (
-            <div>
-                <ul>
+            <div
+                style={{
+                    overflow: `visible`
+                }}
+            >
+                <h1>Productes de la categoria 2...</h1>
+                <Masonry
+                    elementType={'ul'}
+                >
                     {   this.props.data.subcategoriaPRODUCTES.map(
                             (v,i,a) => {
-                                <li>Referencia: {v.referencia} - Nombre: {v.descripcion}</li>
+                                console.log(v);
+                                if (i < 20) {
+                                    return (
+                                        <li key={i}
+                                            style={{
+                                                width: `100px`,
+                                                display: `inline-block`
+                                            }}
+                                        >
+                                            Referencia: {v.referencia} - Nombre: {v.descripcion}
+                                            <img
+                                                src={`http://cashflow.colombiaespassion.net/productos/${v.imagen_principal}`}
+                                                alt={v.descripcion}
+                                                title={v.descripcion_long_es}
+                                                style={{
+                                                    width: `100px`
+                                                }}
+                                            />
+                                        </li>
+                                    );
+                                }
+                                return null;
                             }
                         )
                     }
-                </ul>
+                </Masonry>
             </div>
         );
     }
 }
 
-const MostrariAmbProductes = graphql(ProductesQuery)(Mostrari);
+const MostrariAmbProductes = graphql(ProductesQuery, {
+    options: {
+        variables
+    }
+})(Mostrari);
 
 export default class App extends Component {
     constructor(props) {
