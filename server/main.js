d9ff907a5d327d321fbbe19ae0c42b4bae634290
SyBr Len <sybr@systemli.org>
2017-12-20 18:49:11 +0100

    Mailing.

--- server/main.js
+++ server/main.js
@@ -1,5 +1,18 @@
 import { Meteor } from 'meteor/meteor';
+import { Email } from 'meteor/email';
 
 Meteor.startup(() => {
   // code to run on server at startup
+});
+
+Meteor.methods({
+    'enviaCorreu'(from, text) {
+        this.unblock();
+        Email.send({
+            from,
+            to: "ventas@latinmoda.info",
+            subject: "Mensaje de BlusasColombianas.es",
+            text
+        });
+    }
 });
