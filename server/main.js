4668c885f81c7574cd16eba081a22c286b656f06
SyBr Len <sybr@systemli.org>
2017-12-24 01:51:51 +0100

    SEO in place. PhantomJS, Prerender, prerender-node.

--- server/main.js
+++ server/main.js
@@ -1,9 +1,24 @@
 import { Meteor } from 'meteor/meteor';
 import { Email } from 'meteor/email';
+import { WebApp } from 'meteor/webapp';
+import prerenderio from 'prerender-node';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+  prerenderio.set('host', 'localhost:10000');
+  prerenderio.set('protocol', 'http');
+  prerenderio.set('forwardHeaders', true);
+  prerenderio.set('afterRender', function afterRender(error) {
+      if (error) {
+         console.log('prerenderio error', error);
+         return;
+      }
+ });
+ WebApp.rawConnectHandlers.use(prerenderio);
 });
+
+// Meteor.startup(() => {
+//   // code to run on server at startup
+// });
 
 Meteor.methods({
     'enviaCorreu'(from, text) {
