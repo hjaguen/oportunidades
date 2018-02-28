b071511f32f801ae3349f87ad38ed36d6cf7f408
Mauricio Ardila <andresmauricioardila@gmail.com>
2018-02-28 08:00:51 -0500

    lazyload terminado

---
+++ imports/client/ui/Placeholder.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Placeholder() {
+  return (
+    <div>
+     <i className="fa fa-spinner fa-spin" style={{fontSize:`24px`,minHeight:`560`}}></i>
+    </div>
+  );
+}
